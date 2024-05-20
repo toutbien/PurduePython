05e5834ad97bfaece608d98d58e54d3567b6a0b5
gr8goat <90510480+toutbien@users.noreply.github.com>
2024-05-20 12:32:26 -0400

    Update calendardisplay.py

--- calendardisplay.py
+++ calendardisplay.py
@@ -3,3 +3,8 @@ print(calendar.month(2025, 11))
 
 #start on a certain calendar day prior to print
 calendar.setfirstweekday(calendar.MONDAY)
+
+#check if is a leap year
+#print(calendar.isleap(2026))
+#up to but not including 2026
+#print(calendar.leapdays(2022, 2026))  
