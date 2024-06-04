662e173d15e20fcd19993b4e709a115afceba0bb
gr8goat <90510480+toutbien@users.noreply.github.com>
2024-06-04 12:08:51 -0400

    variables.py
    variables practice basic

---
+++ machinelearning/variables.py
@@ -0,0 +1,7 @@
+your_name = 'Mark E. Mark'
+your_age = 40
+your_height = 71 #inches
+your_alien_race = klingon
+
+print(f"Let's introduce you, {your_name}."}
+print(f"{your_name} is a respectable {your_age} and claims to be {your_alien_race}.")
