807a4098897a197301c4d08979d4473237021312
gr8goat <90510480+toutbien@users.noreply.github.com>
2024-06-15 12:29:22 -0400

    Create pathtoutil.py

---
+++ machinelearning/pathtoutil.py
@@ -0,0 +1,8 @@
+#cooperates with util.py
+#If your util.py file is in a different directory, you might need to add that directory to the system path:
+
+import sys
+sys.path.insert(0, '/path/to/directory')
+import util
+
+Replace '/path/to/directory' with the actual path to the directory containing util.py.
