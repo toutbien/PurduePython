b8716c079c88692bd94a8392f9d97ad86734038e
gr8goat <90510480+toutbien@users.noreply.github.com>
2024-08-15 20:22:57 -0400

    Update week1task2.py

--- machinelearning/week1task2.py
+++ machinelearning/week1task2.py
@@ -4,6 +4,10 @@ def matrix_subtraction(A,B):
   rest = [[0 for _ in range (cols_A)] for _ in range(rows_B)]
 
 #CODE HERE 
+if (rows_A == rows_B) and (cols_A == cols_B_
+  for i in range(rows_A):
+    for j in range(cols_A):
+      = A[i][j]
 
 return rest
 
@@ -13,6 +17,6 @@ X = [[12,7,3],
 Y = [[5,8,1],
      [6,7,3],
      [4,5,9]]
-res = ,atrox+sibtractopm(X,y)
+res = matrix_subtraction(X,Y)
 for r in res:
   print(r)
