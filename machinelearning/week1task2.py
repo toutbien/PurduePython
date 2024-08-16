e38898cbab558087f0a1361491301ad425a8efe7
gr8goat <90510480+toutbien@users.noreply.github.com>
2024-08-15 20:00:52 -0400

    Create week1task2.py
    Matrix Subtraction Homework

---
+++ machinelearning/week1task2.py
@@ -0,0 +1,18 @@
+def matrix_subtraction(A,B):
+  rows_A, cols_A = len(A), len(A[0])
+  rows_B, cols_B = len(B), len(B[0])
+  rest = [[0 for _ in range (cols_A)] for _ in range(rows_B)]
+
+#CODE HERE 
+
+return rest
+
+X = [[12,7,3],
+     [4,5,6],
+     [7,8,9]]
+Y = [[5,8,1],
+     [6,7,3],
+     [4,5,9]]
+res = ,atrox+sibtractopm(X,y)
+for r in res:
+  print(r)
